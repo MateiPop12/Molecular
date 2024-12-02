10a8fafc36a255bde11f92d84b4158dca0362743
Matei <mateipop2001@gmail.com>
2024-12-02 12:41:29 +0200

    Added event callbacks

--- Molecular/src/Molecular/App.cpp
+++ Molecular/src/Molecular/App.cpp
@@ -1,16 +1,28 @@
 #include "App.h"
-#include <GLFW/glfw3.h>
 #include "Log.h"
+
+
+#include <GLFW/glfw3.h>
 
 namespace Molecular
 {
 	App::App()
 	{
 		m_window = std::unique_ptr<Window>(Window::Create(WindowProps()));
+		m_window->SetEventCallback(std::bind(&App::OnEvent, this, std::placeholders::_1));
 	}
 	App::~App()
 	{
 	}
+
+	void App::OnEvent(Event &e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(std::bind(&App::OnWindowClose, this, std::placeholders::_1));
+		//dispatcher.Dispatch<WindowResizeEvent>(std::bind(&App::OnWindowResize, this, std::placeholders::_1));
+		MOL_CORE_TRACE("{0}",e);
+	}
+
 	void App::Run() 
 	{
 		while(m_running)
@@ -19,5 +31,11 @@ namespace Molecular
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_window->OnUpdate();
 		}
+	}
+
+	bool App::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_running = false;
+		return true;
 	}
 }
--- Molecular/src/Molecular/App.h
+++ Molecular/src/Molecular/App.h
@@ -2,6 +2,8 @@
 
 #include "Core.h"
 #include "Window.h"
+#include "Events/Event.h"
+#include "Events/ApplicationEvent.h"
 
 namespace Molecular
 {
@@ -12,7 +14,9 @@ namespace Molecular
 		virtual ~App();
 
 		void Run();
+		void OnEvent(Event &e);
 	private:
+		bool OnWindowClose(WindowCloseEvent& e);
 		std::unique_ptr<Window> m_window;
 		bool m_running = true;
 	};
